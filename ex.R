d4a2a7379e45182da8a4f03902c16b75937f364e
MorariMaxim <119452648+MorariMaxim@users.noreply.github.com>
2023-05-16 12:48:35 +0300

    Revert "Add files via upload"
    This reverts commit 2586d07f2e3e12902b35f2ea85b56f147207345e.

--- ex.R
+++ ex.R
@@ -1,33 +1,59 @@
-
-ex1 = function(X,P){
+ex1_1c = function(u,d,n){
   
-  n = length(x)
-  r = runif(1,0,1)
-  s1 = 0;
-  s2 = 0;
-  for (i in 1:n) {
-    s2 = s1+P[i]
-    if(s1<=r && r<s2 ) return(X[i]);
-    s1 = s2;
-  }
+  x = seq(-n,n,0.1);
+  y = dnorm(x,u,d);
+  plot(x,y);
 }
-ex1(1:3,c(0.1,0.2,0.7))
-check = function(x,p){
+ex2_2= function(r,n){
   
-  c = x;
-  for (i in 1:length(x)) {
-    c[i] = 0;
-  }
-  
-  for (i in 1:1000) {
-    r =ex1(x,p)
-    for (j in 1:length(x)) {
-      if(x[j] == r) {
-        c[j] = c[j]+1;
-        break;
-      }
+  return(mean(rt(n,r)));
+}
+ex2_2_comp = function(){
+  for(r in 2:5){
+    n = 100;
+    for(i in 1:4){
+      n=n*10;
+      cat("r=",r," n=",n,"mean=",ex2_2(r,n),"\n");
     }
   }
-  c
 }
-check(1:3,c(0.1,0.2,0.7))
+ex3_2= function(a,l,n,N,z) {
+  u = a/l;
+  s = a/(l^2);
+  ub= z * s/sqrt(n) + u;
+  sum = 0;
+  for(i in 1:N) {
+    x_n = mean(rgamma(n, a,l));
+    if(x_n <= ub) {
+      sum = sum + 1;
+    }
+  }
+  return(sum/N);
+}
+ex3_2_comp = function(a,l){
+  z = -1.5;
+  for(i in 1:3){
+    N = 5000;
+    for(j in 1:3){
+      cat("z=",z," n=",50," N=",N," sum/N=",ex3_2(a,l,50,N,z),"\n"); 
+      N= N*2;
+    }
+    z=z+1.5;
+  }
+}
+ex4_1 = function(n,p,k){
+  
+  e = n*p;
+  v = n*p*(1-p);
+  s = sqrt(v);
+  q = (k-0.5-e)/s;
+  return(pnorm(q));
+}
+ex4_2 = function(n,p,k){
+  
+  e = n*p;
+  v = n*p*(1-p);
+  s = sqrt(v);
+  q = (k+0.5-e)/s; 
+  return(1-pnorm(q));
+}
